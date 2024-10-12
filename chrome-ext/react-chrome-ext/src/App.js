9f56d4071af76a97e9129885fd4be99677ba2f42
adibhar <adibhar20@gmail.com>
2024-10-12 15:23:41 -0500

    Created a basic chrome extension on react.

---
+++ chrome-ext/react-chrome-ext/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Test chrome extension</h1>
+      <p>Popup testing for chrome extension</p>
+    </div>
+  );
+}
+
+export default App;
